c58fc8d5311687e939bd6dca74c761c276605c53
Crisguts <143626155+Crisguts@users.noreply.github.com>
2024-12-06 01:38:46 -0500

    m

--- MunchFast/src/main/java/org/example/Order.java
+++ MunchFast/src/main/java/org/example/Order.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import java.util.List;
+
 public class Order {
+    private List<Item> items;
+    private Customer customer;
+    private Driver driver;
+    private Restaurant restaurant;
+    private String status;
+    private int orderID;
+    private String customerInstructions;
+    private int deliveryTime;
+
+    // Getters and Setters...
+
+    public void updateStatus(String newStatus) {
+        this.status = newStatus;
+    }
 }
