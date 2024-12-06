c924d7bca1585d24088c1f4d56ee35924a39a627
florencekn <2380334@edu.vaniercollege.qc.ca>
2024-12-05 21:08:35 -0500

    made a few foods!

---
+++ MunchFast/src/main/java/org/example/OrangeJuice.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class OrangeJuice extends Item implements Drink{
+    /**
+     * consume() will be used to decrement from the stock inside the database
+     */
+    @Override
+    public void consume() {
+
+    }
+}
---
+++ MunchFast/src/main/java/org/example/Rice.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Rice extends Item implements Side{
+    /**
+     * consume() will be used to decrement from the stock inside the database
+     */
+    @Override
+    public void consume() {
+
+    }
+}
